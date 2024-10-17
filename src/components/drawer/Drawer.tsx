62d5134fd5ecaff865cf0c224d7b71906d213bf4
kamil3397 <kkgolkiper33@gmail.com>
2024-10-17 21:38:30 +0200

    podswietl jesli strona aktywna

--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -1,16 +1,12 @@
 import React, { PropsWithChildren, useState } from 'react';
 import { styled } from '@mui/material/styles';
-import { Box, Drawer as MuiDrawer, CssBaseline, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import HomeIcon from '@mui/icons-material/Home';
-import InfoIcon from '@mui/icons-material/Info';
-import ListIcon from '@mui/icons-material/List';
-import LogoutIcon from '@mui/icons-material/Logout';
-import AddTaskIcon from '@mui/icons-material/AddTask';
-import PersonIcon from '@mui/icons-material/Person';
+import { Box, Drawer as MuiDrawer, CssBaseline, List, Divider, ListItem, ListItemButton, ListItemIcon, ListItemText, Button, IconButton } from '@mui/material'
 import { DrawerAppBar } from './AppBar';
 import { DrawerHeader } from './DrawerHeader';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthContext } from 'context/AuthContext';
+import { MenuList } from './MenuList';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export const DRAWER_WIDTH = 240;
 
@@ -51,7 +47,6 @@ export const Drawer = ({ children }: PropsWithChildren) => {
     };
 
     const handleLogout = async () => {
-
         await logOutClient()
             .then(() => navigate('/'))
             .catch((err) => console.log(err))
@@ -68,6 +63,7 @@ export const Drawer = ({ children }: PropsWithChildren) => {
                     '& .MuiDrawer-paper': {
                         width: DRAWER_WIDTH,
                         boxSizing: 'border-box',
+                        height: '100%'
                     },
                 }}
                 variant="persistent"
@@ -76,72 +72,11 @@ export const Drawer = ({ children }: PropsWithChildren) => {
             >
                 <DrawerHeader handleDrawerClose={handleDrawerClose} />
                 <Divider />
-                <List>
-                    <Link to='/yourTasks' style={{ textDecoration: 'none', color: '#202142' }}>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <HomeIcon style={{ color: '#202142' }} />
-                                </ListItemIcon>
-                                <ListItemText primary={'Home'} />
-                            </ListItemButton>
-                        </ListItem>
-                    </Link>
-                    <Link to='/about' style={{ textDecoration: 'none', color: '#202142' }}>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <InfoIcon style={{ color: '#202142' }} />
-                                </ListItemIcon>
-                                <ListItemText primary={'About'} />
-                            </ListItemButton>
-                        </ListItem>
-                    </Link>
-                    <Link to='/yourTasks' style={{ textDecoration: 'none', color: '#202142' }}>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <ListIcon style={{ color: '#202142' }} />
-                                </ListItemIcon>
-                                <ListItemText primary={'Your Tasks '} />
-                            </ListItemButton>
-                        </ListItem>
-                    </Link>
-                    <Link to='/addTask' style={{ textDecoration: 'none', color: '#202142' }}>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <AddTaskIcon style={{ color: '#202142' }} />
-                                </ListItemIcon>
-                                <ListItemText primary={'Add new task '} />
-                            </ListItemButton>
-                        </ListItem>
-                    </Link>
-                </List>
+                <MenuList />
                 <Divider />
-                <List>
-                    <Link to='/profile' style={{ textDecoration: 'none', color: '#202142' }}>
-                        <ListItem disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <PersonIcon style={{ color: '#202142' }} />
-                                </ListItemIcon>
-                                <ListItemText primary={'Profile'} />
-                            </ListItemButton>
-                        </ListItem>
-                    </Link>
-                    <Link to='/' style={{ textDecoration: 'none', color: '#202142' }}>
-                        <ListItem disablePadding>
-                            <ListItemButton onClick={handleLogout}>
-                                <ListItemIcon>
-                                    <LogoutIcon style={{ color: '#202142' }} />
-                                </ListItemIcon>
-                                <ListItemText primary={'Logout'} />
-                            </ListItemButton>
-                        </ListItem>
-                    </Link>
-                    {/* // * tutaj dodaj wszystkie swoje routy, czyli redirecty */}
-                </List>
+
+                <Button startIcon={<LogoutIcon />} onClick={() => handleLogout()}>Logout</Button>
+
             </MuiDrawer>
             <Main open={open}>
                 {children}
